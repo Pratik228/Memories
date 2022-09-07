84734e2a1dc9fdf6eb81736e70dc1c13c63b421c
Pratik228 <pratikmishra79@gmail.com>
2022-09-07 09:54:53 +0530

    database changes

--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://pratiksdb123:pratiksdb123@cluster0.kn12ddu.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL = ;
 const PORT = process.env.PORT || 5000;
 
 mongoose
